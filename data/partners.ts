465e1a36850d2f2a6aa3a5ffa42fd611df0f6eaa
Okiljon <okiljondadakhonov@gmail.com>
2025-11-04 13:18:18 +0500

    upd: implemented paretners page

---
+++ data/partners.ts
@@ -0,0 +1,38 @@
+export interface Partner {
+  name: string
+  logo?: string
+  website?: string
+}
+
+export const partners: Partner[] = [
+  {
+    name: "University of Business and Science",
+    logo: "/partners/ubs.jpg",
+    website: "https://ubsu.uz",
+  },
+  {
+    name: "English Language Proficiency Competition",
+    logo: "/partners/english-language-proficiency.jpg",
+    website: "https://english-competition.uz",
+  },
+  {
+    name: "Moscow Engineering Physics Institute (MEPhI)",
+    logo: "/partners/mephi.jpg",
+    website: "https://mephi.ru",
+  },
+  {
+    name: "University of Digital Economics and Agrotechnologies (UDEA)",
+    logo: "/partners/udea.jpg",
+    website: "https://udea.uz",
+  },
+  {
+    name: "D. Mendeleev University of Chemical Technology of Russia (Tashkent Branch)",
+    logo: "/partners/mendeleev.jpg",
+    website: "https://muctr.ru",
+  },
+  {
+    name: "Central Asian University (CAU)",
+    logo: "/partners/cau.jpg",
+    website: "https://centralasian.uz/",
+  },
+]
